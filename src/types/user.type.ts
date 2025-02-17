c326dc7c85173ae69ca97f50b402f8ca53424c46
azimuddin2 <mohammadazimuddin274@gmail.com>
2025-02-17 23:51:40 +0600

    UserContext and Providers task add

---
+++ src/types/user.type.ts
@@ -0,0 +1,22 @@
+export interface ClientInfo {
+  device: string;
+  browser: string;
+  ipAddress: string;
+  pcName: string;
+  os: string;
+  userAgent: string;
+}
+
+export interface IUser {
+  clientInfo?: ClientInfo;
+  _id: string;
+  name: string;
+  email: string;
+  role: 'user' | 'admin';
+  hasShop?: boolean;
+  isActive?: boolean;
+  otpToken?: any;
+  lastLogin?: string;
+  createdAt?: string;
+  updatedAt?: string;
+}
