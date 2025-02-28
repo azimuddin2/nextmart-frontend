00ee553dc9460ed656b10c5fc18254b56f3be31a
azimuddin2 <mohammadazimuddin274@gmail.com>
2025-02-28 18:24:05 +0600

    nextmart project redux setup

---
+++ src/redux/features/cartSlice.ts
@@ -0,0 +1,16 @@
+import { IProduct } from '@/types';
+import { createSlice } from '@reduxjs/toolkit';
+
+interface InitialState {
+  products: IProduct[];
+}
+
+const initialState: InitialState = {
+  products: [],
+};
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {},
+});
